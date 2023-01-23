8a5d1028a1dae594a69416df8390cd9a6ec70cda
malachiw <malachiw@umich.edu>
2023-01-23 16:07:16 -0500

    Initial Commit

---
+++ Chefd/ChefdApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefdApp.swift
+//  Chefd
+//
+//  Created by Malachi Wong on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct ChefdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
